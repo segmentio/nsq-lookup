3457b93d54a2032a33f93c00ea945498df27599d
John Chesley <john@chesl.es>
2015-09-17 21:28:44 -0400

    use null instead of [] as err argument if there are no errors

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function lookup(addrs, opts, fn) {
     errors = filter(errors);
     results = dedupe(filter(results));
     debug('errors=%j results=%j', errors, results);
-    fn(errors, results)
+    fn(errors.length ? errors : null, results);
   });
 }
 
